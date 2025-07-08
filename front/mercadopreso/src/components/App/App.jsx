4be6a8e372a87c3767c2506e047eff2db09d23a0
raveljalmeida <ravel.almeida2018@gmail.com>
2025-07-08 14:46:48 -0300

    feat: login e logout finalizados

--- front/mercadopreso/src/components/App/App.jsx
+++ front/mercadopreso/src/components/App/App.jsx
@@ -1,47 +1,95 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import './App.css'
-import Header from '../Header/Header'
-import Login from '../Login/Login'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  useNavigate
+} from 'react-router-dom';
+import './App.css';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
 import Produtos from '../Produtos/Produtos';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export default function App(){
-
+export default function App() {
   const [loginCtrl, setLoginCtrl] = useState(false);
 
   useEffect(() => {
-
     const verifyLogin = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/verify', {
+          withCredentials: true
+        });
 
-      const response = await axios.get('http://localhost:8000/verify', {
-      withCredentials: true
-      });
-
-      if(response.data){
-        setLoginCtrl(true);
-      }else{
+        setLoginCtrl(!!response.data);
+      } catch {
         setLoginCtrl(false);
       }
-
-    }
+    };
 
     verifyLogin();
+  }, []);
 
-  } ,[])
- 
-  return(
-    <>
-      <BrowserRouter>
-        <div id='app'>
-          <Header></Header>
-          <Routes>
-            <Route path='/' element= {<Produtos />} /* {loginCtrl ? <Navigate to="/products" replace/> : <Login loginControle = {() => setLoginCtrl(true)}/>} *//>
-            <Route path='/products' element={loginCtrl ? <Produtos loginControle = {() => setLoginCtrl(false)}/> : <Navigate to="/" replace/>} />
-            <Route path='/login' element={loginCtrl ? <Navigate to="/products" replace/> : <Login loginControle = {() => setLoginCtrl(true)}/>} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </>
-  )
+  return (
+    <BrowserRouter>
+      <AppContent loginCtrl={loginCtrl} setLoginCtrl={setLoginCtrl} />
+    </BrowserRouter>
+  );
+}
+
+function AppContent({ loginCtrl, setLoginCtrl }) {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/logout', {},
+        {withCredentials: true}
+      );
+
+      console.log(response.data);
+
+      setLoginCtrl(false);
+      navigate('/'); 
+    } catch (error) {
+      console.error('Erro ao deslogar:', error);
+    }
+  };
+
+  return (
+    <div id="app">
+      <Header loginCtrl={loginCtrl} setLogin={() => setLoginCtrl(true)} />
+
+      {loginCtrl && (
+        <Link id="logout" onClick={logout}>
+          Sair
+        </Link>
+      )}
+
+      <Routes>
+        <Route path="/" element={<Produtos />} />
+        <Route
+          path="/products"
+          element={
+            loginCtrl ? (
+              <Produtos loginControle={() => setLoginCtrl(false)} />
+            ) : (
+              <Navigate to="/" replace />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            loginCtrl ? (
+              <Navigate to="/products" replace />
+            ) : (
+              <Login loginControle={() => setLoginCtrl(true)} />
+            )
+          }
+        />
+      </Routes>
+    </div>
+  );
 }
--- front/mercadopreso/src/components/Header/Header.jsx
+++ front/mercadopreso/src/components/Header/Header.jsx
@@ -1,28 +1,31 @@
-import './Header.css'
-import Logo from '../../assets/Logo.png'
-import { Link } from 'react-router-dom'
+import './Header.css';
+import Logo from '../../assets/Logo.png';
+import { Link, useLocation } from 'react-router-dom';
 import { useState } from 'react'
+import axios from 'axios';
 
-export default function Header(){
+export default function Header({ loginCtrl, setLogin }) {
 
-    const [ ctrlEntry, setCtrlEntry ] = useState(false);
+  const location = useLocation();
 
-    return (
-        <>
-          <header>
-            <Link to="/" onClick={() => setCtrlEntry(false)}><img src={Logo} id='logo'/></Link>
-            {
-              ctrlEntry ? 
-              <nav>
-              </nav> :
-              <nav>
-                <Link to='/login' onClick={() => setCtrlEntry(true)}>Entrar</Link>
-                <Link>Crie a sua conta</Link>
-              </nav>
-            }
-            
-          </header>
-        </>
-    )
+  return (
+    <header>
+      <Link to="/" >
+        <img src={Logo} id="logo" />
+      </Link>
 
+      {location.pathname !== "/login" && (
+        <nav>
+          {!loginCtrl ? (
+            <>
+              <Link to="/login" >Entrar</Link>
+              <Link to="/criar-conta">Crie a sua conta</Link>
+            </>
+          ) : (
+            <span></span>
+          )}
+        </nav>
+      )}
+    </header>
+  );
 }
